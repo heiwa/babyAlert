3be8b876fa130c25dd648535e3871ea4c6b78fd7
ukwshh-work <ukawa.shohei@tis.co.jp>
2026-06-28 23:46:20 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# babyAlert
+
+eufy Camで赤ちゃんの泣き声を検知したらDiscordに通知する。
---
+++ baby_alert/main.py
@@ -0,0 +1,33 @@
+import discord
+import sys
+import os
+from pathlib import Path
+from flask import Flask, request
+
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+
+print("環境変数一覧:", list(os.environ.keys()))
+ALERT_CHANNEL_NAME="志輝"
+
+# インテントの設定
+intents = discord.Intents.default()
+
+bot = discord.Client(intents=intents)
+bot.run(os.getenv("DISCORD_BOT_TOKEN"))
+
+
+app = Flask(__name__)
+
+@app.route("/baby_cry", methods=["POST"])
+def baby_cry():
+    print("赤ちゃんが泣いてる！")
+
+    for guild in bot.guilds:
+        channel = discord.utils.get(guild.text_channels, name=ALERT_CHANNEL_NAME)
+            if channel:
+                await channel.send("志輝が泣いてる！！！")
+
+    return "ok"
+
+app.run(host="0.0.0.0", port=5000)
+
