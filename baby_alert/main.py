9d9c4908ddde40b4f139e5b309c2efdb60445ba1
ukwshh-work <ukawa.shohei@tis.co.jp>
2026-07-08 19:29:51 +0900

    mention fix

--- baby_alert/main.py
+++ baby_alert/main.py
@@ -33,7 +33,7 @@ async def sendAlert():
     for guild in bot.guilds:
         channel = discord.utils.get(guild.text_channels, name=ALERT_CHANNEL_NAME)
         if channel:
-            await channel.send(f"@{USER_ID} 志輝が泣いてる！！！")
+            await channel.send(f"<@{USER_ID}> 志輝が泣いてる！！！")
 
 @bot.event
 async def on_ready():
